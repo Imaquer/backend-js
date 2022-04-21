19c9e45270e003f46a05dc411d99d132489b37c7
juanda <juandacorreo@gmail.com>
2022-04-21 09:06:16 +0200

    before easter

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cervezasRouter = require('./cervezasRouter.js')
+const app = express()
+const path = require('path')
+const staticRoute = path.join(__dirname, 'public')
+
+app.use((req, res, next) => {
+  const now = new Date().toString()
+  console.log(`${now} ${req.method} ${req.url}`)
+  next()
+})
+
+app.use('/static', express.static(staticRoute))
+
+app.use('/cervezas', cervezasRouter)
+
+app.get('/', function (req, res) {
+  res.send('Hola Pepe!')
+})
+
+app.get('/bancos', function (req, res) {
+  res.send('Aquí deberían aparecer los bancos')
+})
+
+app.listen(3000)
