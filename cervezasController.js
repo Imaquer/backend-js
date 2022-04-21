b7f3f7c3a1405694ed4cf5506bd4a8d28c0afb83
juanda <juandacorreo@gmail.com>
2022-04-21 14:32:02 +0200

    db working

--- cervezasController.js
+++ cervezasController.js
@@ -1,5 +1,9 @@
 const cervezasController = {}
 
 cervezasController.list = (req, res) => res.send('Lista de cervezas')
+cervezasController.create = (req, res) =>
+  res.send(
+    `Creada cerveza ${req.body.nombre}. Características: ${req.body.descripción}`
+  )
 
 module.exports = cervezasController
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+// DEPENDENCIAS
 const express = require('express')
 const cervezasRouter = require('./cervezasRouter.js')
+const Cerveza = require('./cervezasModel')
 const app = express()
 const path = require('path')
+require('./db')
 const staticRoute = path.join(__dirname, 'public')
+const bodyParser = require('body-parser')
+
+// MIDDLEWARES
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.use((req, res, next) => {
   const now = new Date().toString()
@@ -12,7 +20,9 @@ app.use((req, res, next) => {
 
 app.use('/static', express.static(staticRoute))
 
-app.use('/cervezas', cervezasRouter)
+app.use('/api/cervezas', cervezasRouter)
+
+//  RUTAS Y ENRUTADORES
 
 app.get('/', function (req, res) {
   res.send('Hola Pepe!')
@@ -22,4 +32,6 @@ app.get('/bancos', function (req, res) {
   res.send('Aquí deberían aparecer los bancos')
 })
 
-app.listen(3000)
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
